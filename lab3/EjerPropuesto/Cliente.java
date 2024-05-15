e37ff4140b0779faae2a3eac56b325cf0bbf8558
AnthonyM <83074409+AnthonyM-ed@users.noreply.github.com>
2024-05-14 22:43:09 -0500

    Create Cliente.java

---
+++ lab3/EjerPropuesto/Cliente.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.net.*;
+
+public class Cliente {
+    // Dirección del servidor (en este caso, la misma máquina)
+    static final String HOST = "localhost";
+    // Puerto en el que el servidor está escuchando
+    static final int PUERTO = 5000;
+
+    public Cliente() {
+        try {
+            // Crear un socket para conectarse al servidor en la dirección y puerto
+            // especificados
+            Socket skCliente = new Socket(HOST, PUERTO);
+
+            // Obtener el flujo de entrada del socket para recibir datos del servidor
+            InputStream aux = skCliente.getInputStream();
+            DataInputStream flujo = new DataInputStream(aux);
+
+            // Leer el mensaje enviado por el servidor y mostrarlo en la consola
+            System.out.println(flujo.readUTF());
+
+            // Cerrar la conexión con el servidor
+            skCliente.close();
+        } catch (Exception e) {
+            // En caso de error, imprimir el mensaje de excepción
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] arg) {
+        // Crear una nueva instancia del cliente, lo que iniciará el proceso de conexión
+        // y recepción de datos
+        new Cliente();
+    }
+}
