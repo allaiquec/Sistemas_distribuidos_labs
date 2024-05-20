97fbf3d61b8b1c243e3f3f0c8834c18a28e7c29f
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2024-05-20 11:28:04 -0500

    comentarios CalculatorServer.java

--- lab4/CalculatorServer.java
+++ lab4/CalculatorServer.java
@@ -1,14 +1,21 @@
 import java.rmi.Naming;
+
+// Clase que representa el servidor RMI para el servicio de calculadora.
 public class CalculatorServer {
-public CalculatorServer() {
-try {
-Calculator c = new CalculatorImpl();
-Naming.rebind("rmi://localhost:1099/CalculatorService", c);
-} catch (Exception e) {
-System.out.println("Trouble: " + e);
-}
-}
-public static void main(String args[]) {
-new CalculatorServer();
-}
+
+    // Constructor que configura el servicio de calculadora y lo registra en el RMI registry.
+    public CalculatorServer() {
+        try {
+            Calculator c = new CalculatorImpl();
+            // Registro del servicio en el RMI registry con el nombre "CalculatorService".
+            Naming.rebind("rmi://localhost:1099/CalculatorService", c);
+        } catch (Exception e) {
+            System.out.println("Trouble: " + e);
+        }
+    }
+
+    // Método principal para iniciar el servidor.
+    public static void main(String[] args) {
+        new CalculatorServer();
+    }
 }
