7bd71bf58c78368298974a6b8cec687c56327007
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2024-05-20 11:29:18 -0500

    comentarios CalculatorClient.java

--- lab4/CalculatorClient.java
+++ lab4/CalculatorClient.java
@@ -1,37 +1,33 @@
-import java.net.MalformedURLException;
 import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
+
+// Clase que representa el cliente RMI para consumir el servicio de calculadora.
 public class CalculatorClient {
-public static void main(String[] args) {
-int num1 = Integer.parseInt(args[0]);
-int num2 = Integer.parseInt(args[1]);
-try {
-Calculator c = (Calculator) Naming.lookup("rmi://localhost/CalculatorService");
-System.out.println( "The substraction of "+num1 +" and "+num2 +" is: "+ c.sub(num1, num2) );
-System.out.println( "The addition of "+num1 +" and "+ num2 +"is: "+c.add(num1, num2) );
-System.out.println( "The multiplication of "+num1 +" and "+num2 +" is: "+c.mul(num1, num2) );
-System.out.println( "The division of "+num1 +" and "+ num2 +"is: "+c.div(num1, num2) );
+
+    // Método principal para ejecutar el cliente.
+    public static void main(String[] args) {
+        // Verificación de argumentos.
+        if (args.length < 2) {
+            System.out.println("Please provide two integers as arguments.");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        try {
+            // Búsqueda del servicio remoto en el RMI registry.
+            Calculator c = (Calculator) Naming.lookup("rmi://localhost/CalculatorService");
+
+            // Invocación de los métodos remotos y muestra de los resultados.
+            System.out.println("The addition of " + num1 + " and " + num2 + " is: " + c.add(num1, num2));
+            System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " + c.sub(num1, num2));
+            System.out.println("The multiplication of " + num1 + " and " + num2 + " is: " + c.mul(num1, num2));
+            System.out.println("The division of " + num1 + " and " + num2 + " is: " + c.div(num1, num2));
+        } catch (Exception e) {
+            // Manejo de excepciones con mensajes de error.
+            System.out.println("Exception: " + e);
+        }
+    }
 }
-catch (MalformedURLException murle) {
-System.out.println();
-System.out.println("MalformedURLException");
-System.out.println(murle);
-}
-catch (RemoteException re) {
-System.out.println();
-System.out.println("RemoteException");
-System.out.println(re);
-}
-catch (NotBoundException nbe) {
-System.out.println();
-System.out.println("NotBoundException");
-System.out.println(nbe);
-}
-catch (java.lang.ArithmeticException ae) {
-System.out.println();
-System.out.println("java.lang.ArithmeticException");
-System.out.println(ae);
-}
-}
+
 }
