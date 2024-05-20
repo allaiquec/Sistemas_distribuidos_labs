bd252363afd327ee797496fa9d315758e9337b86
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2024-05-20 11:27:04 -0500

    comentarios Calculator.java

--- lab4/Calculator.java
+++ lab4/Calculator.java
@@ -1,6 +1,17 @@
-public interface Calculator extends java.rmi.Remote {
-    public int add(int a, int b) throws java.rmi.RemoteException;
-    public int sub(int a, int b) throws java.rmi.RemoteException;
-    public int mul(int a, int b) throws java.rmi.RemoteException;
-    public int div(int a, int b) throws java.rmi.RemoteException;
-    }
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+// Definición de la interfaz remota para el servicio de calculadora.
+public interface Calculator extends Remote {
+    // Método para sumar dos números.
+    int add(int a, int b) throws RemoteException;
+    
+    // Método para restar dos números.
+    int sub(int a, int b) throws RemoteException;
+    
+    // Método para multiplicar dos números.
+    int mul(int a, int b) throws RemoteException;
+    
+    // Método para dividir dos números.
+    int div(int a, int b) throws RemoteException;
+}
