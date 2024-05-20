46b34d13fd613c87511310f4006cf02ce5209753
HeidiCordova <88119136+HeidiCordova@users.noreply.github.com>
2024-05-20 11:27:39 -0500

    comentarios CalculatorImpl.java

--- lab4/CalculatorImpl.java
+++ lab4/CalculatorImpl.java
@@ -1,24 +1,39 @@
-public class CalculatorImpl extends java.rmi.server.UnicastRemoteObject implements Calculator {
-    // Implementations must have an explicit constructor
-    // in order to declare the RemoteException exception
-    public CalculatorImpl()
-    throws java.rmi.RemoteException {
-    super();
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+// Implementación de la interfaz remota Calculator.
+public class CalculatorImpl extends UnicastRemoteObject implements Calculator {
+
+    // Constructor que declara RemoteException.
+    protected CalculatorImpl() throws RemoteException {
+        super();
     }
-    public int add(int a, int b)
-    throws java.rmi.RemoteException {
-    return a + b;
+
+    // Implementación del método para sumar dos números.
+    @Override
+    public int add(int a, int b) throws RemoteException {
+        return a + b;
     }
-    public int sub(int a, int b)
-    throws java.rmi.RemoteException {
-    return a - b;
+
+    // Implementación del método para restar dos números.
+    @Override
+    public int sub(int a, int b) throws RemoteException {
+        return a - b;
     }
-    public int mul(int a, int b)
-    throws java.rmi.RemoteException {
-    return a * b;
+
+    // Implementación del método para multiplicar dos números.
+    @Override
+    public int mul(int a, int b) throws RemoteException {
+        return a * b;
     }
-    public int div(int a, int b)
-    throws java.rmi.RemoteException {
-    return a / b;
+
+    // Implementación del método para dividir dos números.
+    @Override
+    public int div(int a, int b) throws RemoteException {
+        // Verificación de división por cero.
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
     }
-    }
+}
